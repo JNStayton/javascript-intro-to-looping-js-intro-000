b5518bca4f611da493fad818147bf136325a9a0c
Jessica Stayton <jessica.stayton@gmail.com>
2021-03-30 23:17:39 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,6 +2,6 @@ function forLoop(array) {
   for (let i = 0; i < 25; i++) {
   if (i === 1)
     array.push(`I am 1 strange loop`);
-  else
+  else if ( i === 0 || 1 < i < 25)
     array.push(`I am ${i} strange loops`);
 }
